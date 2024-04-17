76165f40a1533bc0599a5f99d06a0b01f75e85f2
fidaa01 <fidafathi7869@gmail.com>
2024-04-17 09:22:52 +0530

    second commit

---
+++ hello2.py
@@ -0,0 +1,11 @@
+def greet():
+    name = input("What's your name? ")
+    print("Hello, " + name + "! Welcome to the world of Python.")
+
+def main():
+    print("This program is being tested or updated.")
+    greet()
+
+if __name__ == "__main__":
+    main()
+
